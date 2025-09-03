eca9a325f213746496923cb575be7606a77900b6
Martin Selin <seleeto@gmail.com>
2025-09-03 18:14:46 +0200

    Nu håller vi för idag

--- Arrayer/8UserInfo/Program.cs
+++ Arrayer/8UserInfo/Program.cs
@@ -1,3 +1,44 @@
-﻿using Users;
+﻿using System;
+using Users;
 
+
+
+
+User[] users = new User[5];
+int count = 0;
+bool fortsatt = true;
+
+do
+{
+    Console.Write("Mata in ditt namn: ");
+    string namn = Console.ReadLine();
+
+    Console.Write("Mata in din adress: ");
+    string adress = Console.ReadLine();
+
+    Console.Write("Mata in ditt postnr: ");
+    int postnummer = int.Parse(Console.ReadLine());
+
+    Console.Write("Mata in din postort: ");
+    string postort = Console.ReadLine();
+
+    users[count] = new User(namn, adress, postnummer, postort);
+    count++;
+
+    if (count < 5)
+    {
+        Console.Write("Vill du mata in fler? (Y/N): ");
+        char svaret = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+        if (char.ToUpper(svaret) == 'N')
+            fortsatt = false;
+    }
+}
+while (fortsatt && count < 5);
+
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine($"{users[i].Namn}, {users[i].Adress}, {users[i].Postnummer}, {users[i].Postort} ");
+    Console.WriteLine();
+}
 
--- Arrayer/8UserInfo/Users.cs
+++ Arrayer/8UserInfo/Users.cs
@@ -5,9 +5,9 @@ namespace Users
         public string Namn { get; set; }
         public string Adress { get; set; }
         public int Postnummer { get; set; }
-        public int Postort { get; set; }
+        public string Postort { get; set; }
 
-        public User(string namn, string adress, int postnummer, int postort)
+        public User(string namn, string adress, int postnummer, string postort)
         {
             Namn = namn;
             Adress = adress;
