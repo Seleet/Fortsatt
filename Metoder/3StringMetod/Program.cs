fc596b268123f002280ccac213ffccbecf3b9469
Martin Selin <seleeto@gmail.com>
2025-09-04 22:52:57 +0200

    kattmannen

--- Metoder/3StringMetod/Program.cs
+++ Metoder/3StringMetod/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        string katt = "katt";
+        string mannen = "mannen";
+        System.Console.WriteLine($"{StringCombiner(katt, mannen)}");
+
+    }
+    public static string StringCombiner(string Ord1, string Ord2)
+    {
+        string ord1 = Ord1;
+        string ord2 = Ord2;
+        string dubbelstring = ord1 + ord2;
+        return dubbelstring;
+
+    }
+}
