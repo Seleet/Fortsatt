bf86b96ae50a180786ffe3f237c9992266099b6a
Martin Selin <seleeto@gmail.com>
2025-09-04 16:49:41 +0200

    test

---
+++ Metoder/2Percentage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Mata in ett decimaltal mellan 0 och 1!");
+        string input = Console.ReadLine();
+
+        if (decimal.TryParse(input, NumberStyles.Number, CultureInfo.InvariantCulture, out decimal value))
+        {
+            Console.WriteLine($"input blir {ToPercentage(value)}%");
+        }
+        else
+        {
+            Console.WriteLine("Ogiltig inmatning, försök igen.");
+        }
+    }
+
+    static decimal ToPercentage(decimal talet)
+    {
+        decimal resultat = talet * 100;
+        return resultat;
+    }
+}
