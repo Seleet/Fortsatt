83d9f9041b885b951b5253626d054e21a8963f62
Martin Selin <seleeto@gmail.com>
2025-09-04 23:09:02 +0200

    1337-moms

--- Metoder/4Momsen/Program.cs
+++ Metoder/4Momsen/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main()
+    {
+        int vissSumma = 1337;
+        System.Console.WriteLine($"Momsen på {vissSumma} är {Momsen(vissSumma)}");
+    }
+    public static decimal Momsen(int Summan)
+    {
+
+        decimal momsen = Summan * 0.25m;
+        return momsen;
+    }
+}
