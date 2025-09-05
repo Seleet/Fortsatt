5c2a982d38082c37f70df70b8fa88a906074521e
Martin Selin <seleeto@gmail.com>
2025-09-05 10:35:18 +0200

    albatross

---
+++ Metoder/7Lagstaord/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+
+        string[] enListaStrings = { "conny", "albatross", "sax", "kamel", "ost" };
+
+        Console.WriteLine("Längsta ordet är: " + HittaLangstaOrdet(enListaStrings));
+    }
+
+    static string HittaLangstaOrdet(string[] Meningar)
+    {
+        string langtOrd = Meningar[0];
+
+        for (int i = 1; i < Meningar.Length; i++)
+        {
+            if (Meningar[i].Length > langtOrd.Length)
+            {
+                langtOrd = Meningar[i];
+            }
+        }
+
+        return langtOrd;
+    }
+}
