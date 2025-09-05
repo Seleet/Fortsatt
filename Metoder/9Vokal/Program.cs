7019a2ff023de32b1bcac4315ae684d73612d428
Martin Selin <seleeto@gmail.com>
2025-09-05 17:55:00 +0200

    snabba svar

---
+++ Metoder/9Vokal/Program.cs
@@ -0,0 +1,41 @@
+﻿
+class Program
+
+{
+
+    static void Main()
+    {
+
+        Console.WriteLine("Skriv in en bokstav: ");
+        char bokstav = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        if (IsVowel(bokstav))
+        {
+            Console.WriteLine($"{bokstav} är en vokal");
+        }
+        else
+        {
+            Console.WriteLine($"{bokstav} är en konsonant");
+
+        }
+
+    }
+
+
+
+    static bool IsVowel(char bokstav)
+    {
+        char[] vokaler = { 'a', 'e', 'i', 'o', 'u', 'y', 'å', 'ä', 'ö' };
+        char c = char.ToLower(bokstav);
+
+        for (int i = 0; i < vokaler.Length; i++)
+            if (c == vokaler[i])
+                return true;
+
+        return false;
+    }
+
+}
+
+
