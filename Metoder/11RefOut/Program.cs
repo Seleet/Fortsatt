9e829fd9122df55686948aff21dd2bfc9757934a
Martin Selin <seleeto@gmail.com>
2025-09-05 23:32:43 +0200

    ref och out

---
+++ Metoder/11RefOut/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+static void ReferenceOut(ref int parraRef, out int parraUt)
+{
+    parraRef += 100;
+    parraUt = 1337;
+}
+int value1 = 15;
+int value2;
+
+ReferenceOut(ref value1, out value2);
+
+Console.WriteLine($"value1 efter: {value1}");
+Console.WriteLine($"value2 efter: {value2}");
