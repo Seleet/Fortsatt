f95dd51beb0d4bcc84e31d73a9ed20f890ef23ee
Martin Selin <seleeto@gmail.com>
2025-09-05 12:17:33 +0200

    lemonzqueezy

---
+++ Metoder/8SumMultiply/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+int[] heltal = new int[] { 1, 2, 3, 4};
+
+
+Console.WriteLine($"Adderat blir det {Sum(heltal)}, och multiplicerat blir det {Multiply(heltal)}");
+Sum(heltal);
+Multiply(heltal);
+
+static int Sum(int[] Heltal)
+{
+    int summerat = 0;
+    for (int i = 0; i < Heltal.Length; i++)
+    {
+        summerat += Heltal[i];
+    }
+     return summerat;
+
+}
+
+static int Multiply(int [] Heltal)
+{
+    int multiplicerat = 1;
+    for (int i = 0; i < Heltal.Length; i++)
+    {
+        multiplicerat *= Heltal[i];
+
+
+    }
+    return multiplicerat;
+}
